97dd305d11764f182b1e0e9ae343c1f95c211309
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-28 21:10:32 -0700

    Created Instafollow class and created the Selenium driver

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from selenium import webdriver
+
+
 IG_USERNAME = "ENTER USERNAME"
 IG_PASS = "ENTER PASSWORD!"
 SIMILAR_ACCOUNT = "ACCOUNT YOU TO FOLLOW ON IG"
 CHROME_DRIVER_PATH = "ENTER CHROME DRIVER PATH"
+
+
+class InstaFollow:
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)

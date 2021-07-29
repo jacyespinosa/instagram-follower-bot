2b2563ec4eff76205e15997ab573db48c275cef2
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-28 21:35:40 -0700

    Log in to instagram account. Added an exception just in case the page may load slowly so it keep trying to find that element and not break the code.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 
 IG_USERNAME = "ENTER USERNAME"
@@ -10,3 +12,22 @@ CHROME_DRIVER_PATH = "ENTER CHROME DRIVER PATH"
 class InstaFollow:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+
+    def login(self):
+        website = self.driver.get('https://www.instagram.com/accounts/login/')
+        time.sleep(5)
+        '''
+        LOG IN TO INSTAGRAM ACCOUNT. PAGES MAY LOAD SLOW, THEREFORE, IT IS SAFE TO ADD AN EXCEPTION TO PUT A SLEEP TIMER
+        AND TRY ACCESSING THE ELEMENT AGAIN.
+        '''
+        try:
+            enter_username = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+            enter_username.send_keys(f'{IG_USERNAME}')
+            enter_pass = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+            enter_pass.send_keys(f'{IG_PASS}')
+        except NoSuchElementException:
+            time.sleep(1)
+
+        log_in = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
+        log_in.click()
+        time.sleep(5)
